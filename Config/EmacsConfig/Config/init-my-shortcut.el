2ca8a3972a6666e03c69b7ccad502dc8c395c94c
guodong <smile_guodong@163.com>
2014-11-13 14:13:28 +0800

    mv tools to cloud

--- Config/EmacsConfig/Config/init-my-shortcut.el
+++ Config/EmacsConfig/Config/init-my-shortcut.el
@@ -9,12 +9,14 @@
 
 ;; shortcut for my toolkit
 (eval-after-load "em-alias"
-  '(progn (eshell/alias "cd-toolkit" "cd ~/Documents/MyToolkit")
-	  (eshell/alias "cd-config"  "cd ~/Documents/MyToolkit/Config")
-	  (eshell/alias "cd-tools"   "cd ~/Documents/MyToolkit/Tools")
-	  (eshell/alias "cd-res"     "cd ~/Documents/MyToolkit/Resource")	  
+  '(progn (eshell/alias "cd-cloud"   "cd ~/Documents/MyCloud")
+	  (eshell/alias "cd-toolkit" "cd ~/Documents/MyCloud/360Cloud/MyToolkit")
+	  (eshell/alias "cd-test"    "cd ~/Documents/MyCloud/360Cloud/MyTestProject")	  	  
+	  (eshell/alias "cd-core"    "cd ~/Documents/MyCore")
+	  (eshell/alias "cd-config"  "cd ~/Documents/MyCore/Config")
+	  (eshell/alias "cd-doc"     "cd ~/Documents/MyCore/Document")
 	  (eshell/alias "cd-project" "cd ~/Documents/MyProject")
-	  (eshell/alias "goagent"    "python ~/Documents/MyToolkit/GoAgent/MyGoAgent.py $*")
+	  (eshell/alias "goagent"    "python ~/Documents/MyCloud/360Cloud/MyToolkit/GoAgent/MyGoAgent.py $*")
 	  ))
 
 (provide 'init-my-shortcut)
