95cda5df9288f284c9defb011ac9ab6d3b06c239
guodong <smile_guodong@163.com>
2014-11-24 11:50:35 +0800

    add dot_emacs.local config

--- Config/EmacsConfig/Config/init-my-shortcut.el
+++ Config/EmacsConfig/Config/init-my-shortcut.el
@@ -10,11 +10,11 @@
 ;; shortcut for my toolkit
 (eval-after-load "em-alias"
   '(progn (eshell/alias "cd-cloud"   "cd ~/Documents/MyCloud")
-	  (eshell/alias "cd-toolkit" "cd ~/Documents/MyCloud/360Cloud/MyToolkit")
 	  (eshell/alias "cd-test"    "cd ~/Documents/MyCloud/360Cloud/MyTestProject")	  	  
 	  (eshell/alias "cd-core"    "cd ~/Documents/MyCore")
 	  (eshell/alias "cd-config"  "cd ~/Documents/MyCore/Config")
 	  (eshell/alias "cd-doc"     "cd ~/Documents/MyCore/Document")
+	  (eshell/alias "cd-toolkit" "cd ~/Documents/MyToolkit")	  
 	  (eshell/alias "cd-project" "cd ~/Documents/MyProject")
 	  (eshell/alias "goagent"    "python ~/Documents/MyCloud/360Cloud/MyToolkit/GoAgent/MyGoAgent.py $*")
 	  ))
