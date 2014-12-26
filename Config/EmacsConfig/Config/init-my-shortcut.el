f1dd77bd0e7c385c53cb4b0140cbf2d21e5c4e86
guodong <smile_guodong@163.com>
2014-12-26 11:39:08 +0800

    improve zshrc config

--- Config/EmacsConfig/Config/init-my-shortcut.el
+++ Config/EmacsConfig/Config/init-my-shortcut.el
@@ -4,7 +4,7 @@
   '(progn (eshell/alias "ll" "ls -lh $*")
 	  (eshell/alias "la" "ls -a $*")
 	  (eshell/alias "lla" "ls -alh $*")
-	  (eshell/alias "open" "find-file $*")
+	  (eshell/alias "ff"  "find-file $*")
 	  ))
 
 
