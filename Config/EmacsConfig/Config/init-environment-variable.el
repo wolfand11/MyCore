d9e391e9e51394e63aebd1c39df447f6c5a0c3d2
guodong <smile_guodong@163.com>
2015-12-04 13:26:19 +0800

    improve env setting

--- Config/EmacsConfig/Config/init-environment-variable.el
+++ Config/EmacsConfig/Config/init-environment-variable.el
@@ -7,23 +7,17 @@
 
 (let ((common-path)
       (special-path)
-      (env-str (getenv "PATH"))
-      )
-  (when (string-equal system-type "darwin")   ;; osx
-    (setq special-path '(
-                  "/opt/local/bin"
-                  "/opt/local/sbin"
-                  )))
+      (env-str (getenv "PATH")))
+  (when (string-equal system-type "darwin") ;; osx
+    (setq special-path '("/usr/local/bin" "/opt/local/bin" "/opt/local/sbin")))
   (when (string-equal system-type "windows-nt")
-    (setq special-path (list
-                         (expand-file-name (AppendToEmacsResourcePath "diffutils/bin"))
-                         )))
+    (setq special-path (list (expand-file-name (AppendToEmacsResourcePath "diffutils/bin")))))
   (when (string-equal system-type "gnu/linux")
-    (setq special-path '(
-                         )))
-  (setq env-str (concat (mapconcat 'identity special-path path-separator) path-separator env-str))
+    (setq special-path '()))
+  (setq env-str (concat (mapconcat 'identity special-path path-separator)
+                        path-separator
+                        env-str))
   (setenv "PATH" env-str)
-  (setq exec-path (append special-path exec-path))
-  )
+  (setq exec-path (append special-path exec-path)))
 
 (message "init-env-variable SUCCESSFULL!")
