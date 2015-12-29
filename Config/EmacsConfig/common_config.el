afdc7fe3e9b7bee6cc2b9ffb0e75557efe273b8f
guodong <smile_guodong@163.com>
2015-12-29 18:36:00 +0800

    improve emacs config

--- Config/EmacsConfig/common_config.el
+++ Config/EmacsConfig/common_config.el
@@ -1,16 +1,3 @@
 ;; -*- Emacs-Lisp -*-
-
-;; init-user-infomation
-(load "init-user-infomation.el")
-;; init-environment-variable
-(load "init-environment-variable.el")
-
-(load (concat user-emacs-directory "init.el"))
-
-;; ; init-utility-function
-(load "init-utility-function")
-;; init-my-toolkit
-(load "init-my-shortcut")
-
 (put 'upcase-region 'disabled nil)
 
