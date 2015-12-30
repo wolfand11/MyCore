055884f8c89b69c419416bc148f52d2f2c216d28
unknown <guodong@CC-PC.mengguo.gamed9.com>
2015-12-30 20:31:56 +0800

    bug fixed for windows

--- Config/EmacsConfig/dot.emacs.el
+++ Config/EmacsConfig/dot.emacs.el
@@ -1,5 +1,13 @@
 ;; .emacs config
 ;; check os type and load config
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;;(package-initialize)
+
 (message (concat "==== OS-TYPE IS "
                  (symbol-name system-type)
                  " ===="))
