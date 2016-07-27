635a07dfa8f368b1bca7d3234bdd8b2b732d50ab
Guo Dong <smile_guodong@163.com>
2016-07-27 11:20:12 +0800

    add c sharp

--- Config/EmacsConfig/dot.spacemacs.el
+++ Config/EmacsConfig/dot.spacemacs.el
@@ -35,6 +35,7 @@
      emacs-lisp
      javascript
      python
+     csharp
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
