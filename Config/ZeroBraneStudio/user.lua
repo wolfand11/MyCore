fc39eaea8e33b8df766c6cd766e9b8fb0c69fe85
wolfand11 <smile_guodong@163.com>
2017-06-17 20:24:05 +0800

    add new config

---
+++ Config/ZeroBraneStudio/user.lua
@@ -0,0 +1,19 @@
+--[[--
+  Use this file to specify **System** preferences.
+  Review [examples](+D:\Applications\ZeroBraneStudio\cfg\user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
+--]]--
+editor.fontname = "Source Code Pro"
+editor.showfncall = false
+editor.tabwidth = 4
+editor.usetabs = true
+editor.autotabs = true
+editor.usewrap = false
+editor.smartindent = true
+
+-- updated shortcuts to use them as of v1.20
+keymap[ID.STARTDEBUG]       = "F5"
+keymap[ID.STOPDEBUG]        = "Shift-F5"
+keymap[ID.BREAKPOINTTOGGLE] = "F9"
+keymap[ID.STEP]             = "F11"
+keymap[ID.STEPOVER]         = "F10"
+keymap[ID.STEPOUT]          = "Shift-F11"
